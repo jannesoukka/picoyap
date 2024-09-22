a450984676e6ddffa0975ef0e00a1f26ef88591c
Janne Soukka <120817409+jannesoukka@users.noreply.github.com>
2024-09-22 18:04:38 +0300

    The most basic working version and the addition of .gitignore

---
+++ app.py
@@ -0,0 +1,14 @@
+from sqlalchemy.sql import text
+from flask import Flask
+from flask import render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from os import getenv
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.secret_key = getenv("SECRET_KEY")
+db = SQLAlchemy(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
